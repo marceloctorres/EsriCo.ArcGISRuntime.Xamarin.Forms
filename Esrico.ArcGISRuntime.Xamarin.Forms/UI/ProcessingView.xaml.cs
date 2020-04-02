e8313176dc74e5db7f692cc845a7186295f83909
Marcelo Cesar Torres <mtorres@esri.co>
2020-04-01 22:20:42 -0500

    v.1.2.4.1 Upgrade Nuget Packages

--- Esrico.ArcGISRuntime.Xamarin.Forms/UI/ProcessingView.xaml.cs
+++ Esrico.ArcGISRuntime.Xamarin.Forms/UI/ProcessingView.xaml.cs
@@ -5,55 +5,55 @@ using Xamarin.Forms.Xaml;
 
 namespace EsriCo.ArcGISRuntime.Xamarin.Forms.UI
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ProcessingView : ContentView
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class ProcessingView : ContentView
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    public static readonly BindableProperty IsProcessingProperty = BindableProperty.Create(
+      nameof(IsProcessing),
+      typeof(bool),
+      typeof(ProcessingView),
+      defaultValue: false);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool IsProcessing
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public static readonly BindableProperty IsProcessingProperty = BindableProperty.Create(
-          nameof(IsProcessing),
-          typeof(bool),
-          typeof(ProcessingView),
-          defaultValue: false);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool IsProcessing
-        {
-            get => (bool)GetValue(IsProcessingProperty);
-            set => SetValue(IsProcessingProperty, value);
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public static readonly BindableProperty ProcessingMessageProperty = BindableProperty.Create(
-          nameof(ProcessingMessage),
-          typeof(string),
-          typeof(ProcessingView));
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public string ProcessingMessage
-        {
-            get => (string)GetValue(ProcessingMessageProperty);
-            set => SetValue(ProcessingMessageProperty, value);
-        }
-
-        public ProcessingView()
-        {
-            try
-            {
-                InitializeComponent();
-                ProcessingMessage = AppResources.ProcessingMessageLabelText;
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+      get => (bool)GetValue(IsProcessingProperty);
+      set => SetValue(IsProcessingProperty, value);
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static readonly BindableProperty ProcessingMessageProperty = BindableProperty.Create(
+      nameof(ProcessingMessage),
+      typeof(string),
+      typeof(ProcessingView));
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string ProcessingMessage
+    {
+      get => (string)GetValue(ProcessingMessageProperty);
+      set => SetValue(ProcessingMessageProperty, value);
+    }
+
+    public ProcessingView()
+    {
+      try
+      {
+        InitializeComponent();
+        ProcessingMessage = AppResources.ProcessingMessageLabelText;
+      }
+      catch(Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
 }
